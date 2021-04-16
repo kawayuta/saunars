9cf5c880dab60680738d88ec17d41051bb2e1ba6
kawayuta <addkawayuta@gmail.com>
2021-04-16 09:20:29 +0900

    init

--- app/controllers/saunas_controller.rb
+++ app/controllers/saunas_controller.rb
@@ -12,8 +12,12 @@ class SaunasController < ApplicationController
   end
 
   def search
-    @q = Sauna.ransack(search_params)
-    @search_saunas = @q.result(distinct: true)
+    if search_params[:name_ja_or_address_cont] == ""
+      @search_saunas = Sauna.all.limit(25)
+    else
+      @q = Sauna.ransack(search_params)
+      @search_saunas = @q.result(distinct: true)
+    end
   end
 
   # GET /saunas/1 or /saunas/1.json
